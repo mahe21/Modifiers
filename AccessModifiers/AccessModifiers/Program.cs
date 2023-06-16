8f89d92d0d07c3923ae314cfc3a1ead5932dafba
Mahesh <Maheshk2104@gmail.com>
2023-06-16 23:34:42 +0530

    [Mahesh]Add: add more access modifiers

--- AccessModifiers/AccessModifiers/Program.cs
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -1,11 +1,25 @@
 ﻿namespace AccessModifiers
 {
-    public class Program
+    public class Program : PrivateProtectedAccessModifier
     {
         static void Main(string[] args)
         {
             PublicModifier publicModifier = new PublicModifier();
             publicModifier.Intro();
+
+            ConstructorsExample constructorsExample = new ConstructorsExample("Mahesh");
+            ConstructorsExample constructorsExamples = new ConstructorsExample();
+
+            PrivateProtectedAccessModifier privateProtectedAccessModifier = new PrivateProtectedAccessModifier(); 
+            Program program = new Program();
+            Console.WriteLine(program.name);
+            Console.ReadLine();
+
+            ProtectedInternalAccessModifier protectedInternalAccessModifier = new ProtectedInternalAccessModifier();
+            Console.WriteLine(protectedInternalAccessModifier.name);
+
+
+            
 
         }
     }
